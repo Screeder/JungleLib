bd8063535c742470f14e4f2199c28dd0fafc6d53
Screeder <luigipart@web.de>
2014-04-25 00:33:33 +0200

    Working Version

---
+++ Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using LeagueSharp;
+using SharpDX;
+
+namespace JungleLib
+{
+
+    public class Jungle
+    {
+        private static bool initJungleMobs = false;
+        private static JungleMobs[] jungleMobs = new JungleMobs[12];
+        private static List<Obj_AI_Minion> jungleMobList = new List<Obj_AI_Minion>();
+
+        public class JungleMobs
+        {
+            public JungleMobs(String name, bool smite, bool buff)
+            {
+                this.name = name;
+                this.smite = smite;
+                this.buff = buff;
+            }
+
+            public String name;
+            public bool smite;
+            public bool buff;
+        }
+
+
+        protected static void DebugMode()
+        {
+            Drawing.OnDraw += Drawing_OnDraw;
+        }
+
+        private static void Drawing_OnDraw(EventArgs args)
+        {
+
+        }
+
+        private static bool ValidJungleMob(Obj_AI_Minion jungleMob)
+        {
+            return jungleMob != null && jungleMob.IsValid && !jungleMob.IsDead && jungleMob.Team == GameObjectTeam.Neutral;
+        }
+
+        public static bool IsBigMob(Obj_AI_Minion jungleBigMob)
+        {
+            foreach (JungleMobs jungleMob in jungleMobs)
+            {
+                if (jungleBigMob.Name.Contains(jungleMob.name))
+                {
+                    return jungleMob.smite;
+                }
+            }
+            return false;
+        }
+
+        public static bool HasBuff(Obj_AI_Minion jungleBigMob)
+        {
+            foreach (JungleMobs jungleMob in jungleMobs)
+            {
+                if (jungleBigMob.Name.Contains(jungleMob.name))
+                {
+                    return jungleMob.buff;
+                }
+            }
+            return false;
+        }
+
+        public static List<Obj_AI_Minion> GetJungleMobs(bool visible, float range)
+        {
+
+            List<Obj_AI_Minion> jungleMobsDelete = new List<Obj_AI_Minion>();
+            List<Obj_AI_Minion> jungleMobs = new List<Obj_AI_Minion>();
+            
+            foreach (Obj_AI_Minion jungleMob in jungleMobList)
+            {
+                if (ValidJungleMob(jungleMob))
+                {
+                    if (visible && jungleMob.IsVisible)
+                    {
+                        jungleMobs.Add(jungleMob);
+                    }
+                    else if (range.CompareTo(0) != 0 &&
+                             Utils.GetDistanceSqr(jungleMob.Position, ObjectManager.Player.Position) <=
+                             Math.Pow(range, 2))
+                    {
+                        jungleMobs.Add(jungleMob);
+                    }
+                    else
+                    {
+                        jungleMobs.Add(jungleMob);
+                    }
+                }
+                else
+                {
+                    jungleMobsDelete.Add(jungleMob);
+                }
+            }
+            foreach (Obj_AI_Minion jungleMob in jungleMobsDelete)
+            {
+                if (!ValidJungleMob(jungleMob))
+                {
+                    jungleMobList.Remove(jungleMob);
+                }
+            }
+            return jungleMobs;
+        }
+
+        static void Obj_AI_Base_OnCreate(GameObject sender, EventArgs args)
+        {
+            if (sender.IsValid && sender.Type == GameObjectType.obj_AI_Minion
+                && sender.Team == GameObjectTeam.Neutral)
+            {
+                jungleMobList.Add((Obj_AI_Minion)sender);
+            }
+        }
+
+        public static void InitJungleMobs()
+        {
+            if (initJungleMobs)
+                return;
+            Obj_AI_Base.OnCreate += Obj_AI_Base_OnCreate;
+            foreach (Obj_AI_Minion objAiMinion in ObjectManager.Get<Obj_AI_Minion>())
+            {
+                Obj_AI_Base_OnCreate(objAiMinion, new EventArgs());
+            }
+            jungleMobs[0] =  new JungleMobs("GreatWraith",  true,   false);
+            jungleMobs[1] =  new JungleMobs("AncientGolem", true,   true);
+            jungleMobs[2] =  new JungleMobs("GiantWolf",    true,   false);
+            jungleMobs[3] =  new JungleMobs("Wraith",       true,   false);
+            jungleMobs[4] =  new JungleMobs("LizardElder",  true,   true);
+            jungleMobs[5] =  new JungleMobs("Golem",        true,   false);
+            jungleMobs[6] =  new JungleMobs("Worm",         true,   true);
+            jungleMobs[7] =  new JungleMobs("Dragon",       true,   false);
+            jungleMobs[8] =  new JungleMobs("YoungLizard",  false,  false);
+            jungleMobs[9] =  new JungleMobs("Wolf",         false,  false);
+            jungleMobs[10] = new JungleMobs("LesserWraith", false,  false);
+            jungleMobs[11] = new JungleMobs("Golem",        false,  false);
+            initJungleMobs = true;
+        }
+    }
+}
---
+++ Utils.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpDX;
+
+class Utils
+{
+    public static bool IsValidVector3(Vector3 vector)
+    {
+        if (vector.X.CompareTo(0.0f) == 0 && vector.Y.CompareTo(0.0f) == 0 && vector.Z.CompareTo(0.0f) == 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    public static bool IsValidFloat(float t)
+    {
+        if (t.CompareTo(0) != 0 && !float.IsNaN(t) && t.CompareTo(float.MaxValue) != 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static Vector3 perpendicular(Vector3 v)
+    {
+        return new Vector3(-v.Z, v.Y, v.X);
+    }
+
+    public static Vector3 perpendicular2(Vector3 v)
+    {
+        return new Vector3(v.Z, v.Y, -v.X);
+    }
+
+    public static Vector3[] CircleCircleIntersection(Vector3 from, Vector3 middlePoint, float width, float distance)
+    {
+        float D = (float)GetDistance(from, middlePoint);
+        float A = (width * width - distance * distance + D * D) / (2 * D);
+        float H = (float)Math.Sqrt(width * width - A * A);
+        Vector3 Direction = middlePoint - from;
+        Direction.Normalize();
+        Vector3 PA = from + A * Direction;
+        Vector3 S1 = PA + H * Direction;
+        Utils.perpendicular(S1);
+        Vector3 S2 = PA - H * Direction;
+        Utils.perpendicular(S2);
+        return new Vector3[] { S1, S2 };
+    }
+
+    public static bool Close(float a, float b, float eps)
+    {
+        if (Utils.IsValidFloat(eps))
+            eps = eps;
+        else
+            eps = (float)1e-9;
+        return Math.Abs(a - b) <= eps;
+    }
+
+    public static double RadianToDegree(double angle)
+    {
+        return angle * (180.0 / Math.PI);
+    }
+
+    public static float Polar(Vector3 v1)
+    {
+        if (Close(v1.X, 0, 0))
+        {
+            float area1 = v1.Y;
+            if (area1 > 0)
+            {
+                return 90;
+            }
+            else if (area1 < 0)
+            {
+                return 270;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        else
+        {
+            float area1 = v1.Y;
+            var theta = (float)RadianToDegree(Math.Atan((area1) / v1.X));
+            if (v1.X < 0)
+            {
+                theta = theta + 180;
+            }
+            if (theta < 0)
+            {
+                theta = theta + 360;
+            }
+            return theta;
+        }
+    }
+
+    public static float AngleBetween(Vector3 self, Vector3 v1, Vector3 v2)
+    {
+        Vector3 p1 = (-self + v1);
+        Vector3 p2 = (-self + v2);
+        float theta = Polar(p1) - Polar(p2);
+        if (theta < 0)
+            theta = theta + 360;
+        if (theta > 180)
+            theta = 360 - theta;
+        return theta;
+    }
+
+    public static double GetDistanceSqr(Vector3 distance1, Vector3 distance2)
+    {
+        float area1 = distance1.Y;
+        float area2 = distance2.Y;
+        double distance = Math.Pow((distance1.X - distance2.X), 2) +
+                            Math.Pow(((area1) - (area2)), 2);
+        return distance;
+    }
+
+    public static double GetDistance(Vector3 distance1, Vector3 distance2)
+    {
+        double distance = GetDistanceSqr(distance1, distance2);
+        distance = Math.Sqrt(distance);
+        return distance;
+    }
+}
